981a49a5c012c25079c927bb7bf7ccff818855ca
KonH <konh@yandex.ru>
2017-04-27 21:22:19 +0700

    Param in ScoreItem for levels, custom groups etc.;

--- Service/Models/ScoreItem.cs
+++ Service/Models/ScoreItem.cs
@@ -5,6 +5,7 @@ namespace LeaderboardService.Models
 		public string Key { get; set; }
 		public string Game { get; set; }
 		public string Version { get; set; }
+		public string Param { get; set; }
 		public int Score { get; set; }
 	}
 }
