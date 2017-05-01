68df558bc3a17ac6f5141e37941a991117b16bdb
KonH <konh@yandex.ru>
2017-05-01 20:11:55 +0700

    Command line args configuration;

--- Service/Program.cs
+++ Service/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 
 namespace LeaderboardService
@@ -6,11 +8,35 @@ namespace LeaderboardService
 
 	public class Program
 	{
+		public static IConfigurationRoot Configuration { get; set; }
+
+		public static Dictionary<string, string> GetSwitchMappings(IReadOnlyDictionary<string, string> configurationStrings)
+		{
+			return configurationStrings.Select(item =>
+				new KeyValuePair<string, string>(
+					"-" + item.Key.Substring(item.Key.LastIndexOf(':') + 1),
+					item.Key))
+					.ToDictionary(
+						item => item.Key, item => item.Value);
+		}
+
 		public static void Main(string[] args)
 		{
-			var urls = new string[]{ "http://*:8080" };
+			var configParams = new Dictionary<string, string> 
+			{
+				{ "Port", "8080" },
+				{ "Env", "Production" }
+			};
+			var configBuilder = new ConfigurationBuilder();
+			configBuilder.AddInMemoryCollection(configParams).AddCommandLine(args, GetSwitchMappings(configParams));
+			Configuration = configBuilder.Build();
+
+			var port = Configuration.GetValue<string>("Port");
+			var env = Configuration.GetValue<string>("Env");
+
+			var urls = new string[]{ "http://*:" + port };
 			var host = new WebHostBuilder()
-				.UseEnvironment("Development")
+				.UseEnvironment(env)
 				.UseKestrel()
 				.UseStartup<Startup>()
 				.UseUrls(urls)
